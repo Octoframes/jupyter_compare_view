aa7d88e046327c1baae554c9ef6789297e2f0c86
christopher-besch <mail@chris-besch.com>
2022-06-10 14:00:28 +0200

    more changes; can't test because PyPi is down

--- jupyter_splitview/sw_cellmagic.py
+++ jupyter_splitview/sw_cellmagic.py
@@ -1,4 +1,5 @@
 import io
+import os
 from base64 import b64decode
 
 from IPython.core import magic_arguments
@@ -7,6 +8,15 @@ from IPython.core.magic import Magics, cell_magic, magics_class
 from IPython.display import display
 from IPython.utils.capture import capture_output
 from PIL import Image
+
+from jinja2 import Template, StrictUndefined
+
+g_cell_id = 0
+
+def compile_template(in_file: str, **variables) -> str:
+    with open(f"{in_file}", "r", encoding="utf-8") as file:
+        template = Template(file.read(), undefined=StrictUndefined)
+    return template.render(**variables)
 
 
 @magics_class
@@ -54,32 +64,7 @@ class SplitViewMagic(Magics):
             im = Image.open(io.BytesIO(imgdata))
             widget_height = im.size[1]
 
-        html_code = """
-    <div id="foo"></div>
-
-    <script>
-        slider = new juxtapose.JXSlider('#foo',
-            [
-                {
-                    src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.TJNo8135ZBrLCojdcdtNoQHaHa%26pid%3DApi&f=1',
-                },
-                {
-                    src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Yh4sZ7LKCEGMYlmmNnxYawHaHa%26pid%3DApi&f=1',
-                }
-            ],
-            {
-                animate: true,
-                showLabels: false,
-                showCredits: false,
-                startingPosition: "50%",
-                makeResponsive: true,
-                // undocumented shit
-                callback: (jx_slider) => {
-                    // remove juxtapose.js link and logo in bottom left corner
-                    jx_slider.slider.removeChild(jx_slider.labCredit);
-                },
-            });
-    </script>
-        """
+        global g_cell_id
+        html_code = compile_template(os.path.join((os.path.dirname(__file__)), "inject.html"), cell_id=g_cell_id, images=out_images_base64, slider_position=slider_position)
+        g_cell_id += 1
         display(HTML(html_code))
-        
